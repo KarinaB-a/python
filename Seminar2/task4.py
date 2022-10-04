dabbff89cf91e7b81fc43f31771f2aa2f7c4eba6
Karina Babakova <taytynovak@mail.ru>
2022-10-04 15:57:07 +0700

    Исправление

--- Seminar2/task4.py
+++ Seminar2/task4.py
@@ -1,4 +1,5 @@
 '''Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.'''
+import random
 def fill_list_numbers(n):
     positions = []
     path = 'file.txt'
@@ -7,12 +8,12 @@ def fill_list_numbers(n):
         positions.append(int(line))
     data.close()
     d = 1
-    i = 0
-    for number in range(- n, n + 1):
+    numbers = []
+    for i in range(n):
+        numbers.append(random.randint(-n, n))
         if i in positions:
-            d = d * number
-        print (i, ": " ,number) 
-        i += 1
+            d = d * numbers[i]
+    print (numbers) 
     return d
 n = int(input('Введите число n = '))
 print(n)
